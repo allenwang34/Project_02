dce38d987162128da2a15beb90f0f7535de6d058
Yiyang Wang Mac <allenwang950321@gmail.com>
2017-07-09 20:10:18 -0700

    Start project 2

---
+++ Project_02/Project_02/anagrams.cpp
@@ -0,0 +1,42 @@
+//
+//  anagrams.cpp
+//  Project_02
+//
+//  Created by Yiyang Wang on 7/9/17.
+//  Copyright © 2017 Yiyang Wang. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <cstring>
+using namespace std;
+
+const int MAXRESULTS   = 20;    // Max matches that can be found
+const int MAXDICTWORDS = 30000; // Max words that can be read in
+
+int main()
+{
+    string results[MAXRESULTS];
+    string dict[MAXDICTWORDS];
+    ifstream dictfile;         // file containing the list of words
+    int nwords;                // number of words read from dictionary
+    string word;
+    
+    dictfile.open("words.txt");
+    if (!dictfile) {
+        cout << "File not found!" << endl;
+        return (1);
+    }
+    
+    nwords = readDictionary(dictfile, dict);
+    
+    cout << "Please enter a string for an anagram: ";
+    cin >> word;
+    
+    int numMatches = recursivePermute(word, dict, nwords, results);
+    if (!numMatches)
+        cout << "No matches found" << endl;
+    else
+        recurPrint(results, numMatches);
+}
