6430be24fa89dee00c377fa3a146f935e7079c30
Yiyang Wang Mac <allenwang950321@gmail.com>
2017-07-11 01:54:58 -0700

    need to figure out how to use loop recursion

--- Project_02/Project_02/anagrams.cpp
+++ Project_02/Project_02/anagrams.cpp
@@ -15,6 +15,13 @@ using namespace std;
 const int MAXRESULTS   = 20;    // Max matches that can be found
 const int MAXDICTWORDS = 30000; // Max words that can be read in
 
+int readDictionary(istream &dictfile, string dict[]);
+int recursivePermute(string word, const string dict[], int size, string results[]);
+void recurPrint(const string results[], int size);
+
+
+
+
 int main()
 {
     string results[MAXRESULTS];
@@ -23,20 +30,38 @@ int main()
     int nwords;                // number of words read from dictionary
     string word;
     
-    dictfile.open("words.txt");
+    dictfile.open("/Users/ouyang/Desktop/words.txt");
     if (!dictfile) {
         cout << "File not found!" << endl;
         return (1);
     }
     
     nwords = readDictionary(dictfile, dict);
+    cout << nwords << " words" << endl;
+    for (int i =0; i < 5; i++)
+        cout << dict[i] << endl;
     
-    cout << "Please enter a string for an anagram: ";
+   /* cout << "Please enter a string for an anagram: ";
     cin >> word;
     
     int numMatches = recursivePermute(word, dict, nwords, results);
     if (!numMatches)
         cout << "No matches found" << endl;
     else
-        recurPrint(results, numMatches);
+        recurPrint(results, numMatches); */
 }
+
+int readDictionary(istream &dictfile, string dict[]) {
+    
+    int counter = 0;
+    
+    for (int i = 0; i < MAXDICTWORDS; i++) {
+        dictfile >> dict[i] ;
+        counter ++;
+    }
+    
+    return counter;
+}
+
+
+
