280f2224d6cb390d3fb3df02cc88bd7032c3a715
AllenWang <allenwang950321@gmail.com>
2017-07-11 22:19:23 -0700

    still have some problem with the permutation. If figure out, this project is almost there.

--- Project_02/Project_02/anagrams.cpp
+++ Project_02/Project_02/anagrams.cpp
@@ -20,7 +20,7 @@ int readDictionary(istream &dictfile, string dict[]);
 int recursivePermute(string word, const string dict[], int size, string results[]);
 void recurPrint(const string results[], int size);
 
-
+int getLoopSize(istream &dictfile, string *dict);
 
 
 int main()
@@ -42,10 +42,14 @@ int main()
     for (int i =0; i < nwords; i++)
         cout << dict[i] << endl;
     
-   /* cout << "Please enter a string for an anagram: ";
+	cout << "Please enter a string for an anagram: ";
     cin >> word;
+	recursivePermute(word, dict, nwords, results);
+	for (int i = 0; i < MAXRESULTS; i++) {
+		cout << results[i] << endl;
+	}
     
-    int numMatches = recursivePermute(word, dict, nwords, results);
+    /*int numMatches = recursivePermute(word, dict, nwords, results);
     if (!numMatches)
         cout << "No matches found" << endl;
     else
@@ -59,24 +63,42 @@ int getLoopSize(istream &dictfile, string *dict) {
 	return counter;
 }
 
-/*void writeString(istream &dictfile, string dict[], int size) {
-	if (size < 0) return;
-	dictfile >> dict[size];
-	writeString(dictfile, dict, size - 1);
-}*/
 
 
 int readDictionary(istream &dictfile, string dict[]) {
 
-	/*int counter = 0;
-	while (dictfile>>dict[counter])
-	{
-	counter++;*/
-	//int counter = 5;
-	//writeString(dictfile, dict, counter);
 	int counter = getLoopSize(dictfile, dict);
 	return counter;
 }
 
 
+void swap(char &a, char &b) {
+	char temp = a;
+	a = b;
+	b = temp;
+}
 
+void printPermute(string prefix, string rest) {
+	if (rest.length() == 0) {
+		cout << prefix << endl;
+	}
+	else {
+		for (int i = 0; i < rest.length() - 1; i++) {
+			prefix += rest[i];
+			rest.erase(i, 1);
+			printPermute(prefix, rest);
+		}
+	}
+}
+
+
+
+int recursivePermute(string word, const string dict[], int size, string results[]) {
+
+	string prefix = "";
+	string rest = "top";
+	printPermute(prefix,rest);
+	return 100;
+
+
+}
